7d8b6b54664bbe602d3f9a428bc42e53b1974fe7
Exachias <asoglieri@yahoo.it>
2018-03-28 14:17:53 +0200

    Updated@28/3_14:17

---
+++ checkSpace.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+total=`du -b *.csv -c | cut -f1 | tail -n1`
+
+if (( $total >= 50000 ));then
+	sum=$total
+	i=0
+	
+	for size in `du -b *.csv | cut -f1`; do
+		if (( $sum <= 50000 )); then
+			break
+		fi
+
+		let "sum-=$size"
+		let "i++"
+	done
+	
+	echo "Elimino i seguenti file: "
+	echo `ls *.csv | head -n$i`
+	rm `ls *.csv | head -n$i`
+else
+	echo "Nessun file da cancellare, la somma dello spazio occupato non supera i 50kB"
+fi
+	
